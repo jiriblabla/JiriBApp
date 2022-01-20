da44e52947dbb5f046fd5ab3a8e8ab073503f4a7
Jiří Blabla <jiriiblabla@gmail.com>
2022-01-20 20:08:35 +0100

    SplashScreen implement

---
+++ app/src/main/java/com/example/jbapp/SplashScreen.kt
@@ -0,0 +1,22 @@
+package com.example.jbapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.content.Intent
+import android.view.WindowManager
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 2000)
+    }
+}
